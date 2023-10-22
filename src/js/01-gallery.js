c4685e8cb395471b1c6143cbb2e05e2340d50267
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2023-10-23 00:59:19 +0200

    0059

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,30 @@
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 
+
+const createImagesListMarkup = (images) => images.map(({ original, preview, description }) => `
+<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>
+`).join("");
+
+const listImages = document.querySelector(".gallery");
+
+console.log(listImages);
+const imagesMarkup = galleryItems ? createImagesListMarkup(galleryItems) : '';
+listImages.insertAdjacentHTML('beforeend', imagesMarkup)
+
+const slider = new SimpleLightbox('.gallery a' , {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
